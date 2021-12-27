13cc3874482bcb788b229f82068debc8f8bd5acf
Almir <almir@example.com>
2021-12-27 03:39:36 +0300

    file for the program is written

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int[] array = { 1, 2, 3, 4 };
+// int[] array = { 1, 3, 14, 5, 7, 1, 23};
+// int[] array = { 2, -4, 16};
+int[] array = { 11, 9, 8, 15};
+// [1, 3, 14, 5, 7, 1, 23] -> [14, 23]
+// [2, -4, 16] -> [16]
+// [11, 9, 8, 15]
+
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+
+    if (count == 0) Console.WriteLine("Пустой массив");
+    else
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write($"{arr[i]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int[] SelectionArr(int[] arr, int find)
+{
+    int[] arrayNew = new int[arr.Length]; ;
+    int m = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > find)
+        {
+            arrayNew[i] = arr[i];
+            m++;
+        }
+    }
+    return arrayNew;
+}
+
+
+//Array.ForEach(array, Console.WriteLine);
+PrintArray(array);
+
+int[] arrayRez = SelectionArr(array, 8);
+
+arrayRez = arrayRez.Where(x => x != 0).ToArray();
+
+Console.WriteLine("Новый массив");
+
+PrintArray(arrayRez);
