6ae3a3ca9ddbb2da747bfd742ece170c1ce1cd92
Almir <almir@example.com>
2022-01-26 06:34:07 +0300

    добавление 2 варианта с презаписью в массивы

--- Program.cs
+++ Program.cs
@@ -1,33 +1,33 @@
 string[] Main()
 {
-  int count = 0; // текущее количество строк в массиве
-  string[] AS = new string[count]; // выделить память для 0 строк
-  string s;
-  string[] AS2; // дополнительная переменная-ссылка - сохраняет старый массив строк
+    int count = 0; // текущее количество строк в массиве
+    string[] AS = new string[count]; // выделить память для 0 строк
+    string s = String.Empty;
+    string[] AS2; // дополнительная переменная-ссылка - сохраняет старый массив строк
 
-  Console.WriteLine("Enter strings:");
+    Console.WriteLine("Enter strings:");
 
-  do
-  {
-    s = Console.ReadLine();
-    if (s!="")
+    do
     {
-      count++;
-      AS2 = new string[count];
-      for (int i = 0; i < AS.Length; i++) // Если использовать AS.Length не работает
+        s = Console.ReadLine();
+        if (s != "")
         {
-          AS2[i] = AS[i];
+            count++;
+            AS2 = new string[count];
+            for (int i = 0; i < AS.Length; i++)
+            {
+                AS2[i] = AS[i];
+            }
+
+            // добавить последнюю введенную строку в массив AS2
+            AS2[count - 1] = s;
+
+            // перенаправить ссылку AS на AS2
+            AS = AS2;
         }
-        
-        // добавить последнюю введенную строку в массив AS2
-        AS2[count - 1] = s;
-        
-        // перенаправить ссылку AS на AS2
-        AS = AS2;
-    }
-  }while (s != "");
-return AS;
-} 
+    } while (s != "");
+    return AS;
+}
 
 
 void PrintArray(string[] arrString)
@@ -39,37 +39,37 @@ void PrintArray(string[] arrString)
     {
         for (int i = 0; i < arrString.Length; i++)
         {
-        Console.WriteLine("array[{0}] = {1}", i, arrString[i]);
-        }   
-    }
-}
-
-
-string[] Selection (string[] arr, int strLength)  //предварительный подсчет элементов результирующего массива
-{
-    int m = 0; //длина результирующего массива
-    string text = String.Empty;
-    for (int i = 0; i < arr.Length; i++)
-    {
-        text = arr[i];
-        if (text.Length <= strLength) m++;
-    }
-    
-    string[] arrayNew = new string[m];
-    
-    m = 0;
-
-    for (int i = 0; i < arr.Length; i++)
-    {
-        text = arr[i];
-        if (text.Length <= strLength)
-        {
-            arrayNew[m] = arr[i];
-            m++;
+            Console.WriteLine("array[{0}] = {1}", i, arrString[i]);
         }
     }
-    return arrayNew;
 }
+
+
+// string[] Selection(string[] arr, int strLength)  //предварительный подсчет элементов результирующего массива
+// {
+//     int m = 0; //длина результирующего массива
+//     string text = String.Empty;
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         text = arr[i];
+//         if (text.Length <= strLength) m++;
+//     }
+
+//     string[] arrayNew = new string[m];
+
+//     m = 0;
+
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         text = arr[i];
+//         if (text.Length <= strLength)
+//         {
+//             arrayNew[m] = arr[i];
+//             m++;
+//         }
+//     }
+//     return arrayNew;
+// }
 
 //["hello", "2", "world", ":-)"] -> ["2", ":-)"]
 //["1234", "1567", "-2", "computer science"] -> ["-2"]
@@ -88,7 +88,41 @@ Console.Write("Введите максимальную длину строк в 
 
 int stringLeng = int.Parse(Console.ReadLine());
 
-string[] arrayRez = Selection(arrayStr, stringLeng);
+//string[] arrayRez = Selection(arrayStr, stringLeng);
+
+//Console.WriteLine("Новый массив с предварительным подсчетом элементов результирующего массива");
+
+//PrintArray(arrayRez);
+
+
+string[] Selection2(string[] arr, int strLength)
+{
+  int count = 0;// длина результируюего массива
+  string[] AS = new string[count];// результирующий массив, заданный в методе но за пределами FOR для того чтобы вернуть из метода массив
+  string s = String.Empty;
+  string[] AS2;
+  
+  for (int i = 0; i < arr.Length; i++)
+  {
+    s = arr[i];
+
+    if (s.Length <= strLength)
+    {
+      count++;
+      AS2 = new string[count];
+      for (int j = 0; j < AS.Length; j++)
+      {
+        AS2[j] = AS[j];
+      }
+      AS2[count - 1] = s;
+
+      AS = AS2;
+    }
+  } 
+  return AS;
+}
+
+string[] arrayRez = Selection2(arrayStr, stringLeng);
 
 Console.WriteLine("Новый массив с предварительным подсчетом элементов результирующего массива");
 
